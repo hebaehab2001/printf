286ee9cf2f9033a12fe33690ae63574d896f1f34
YoustinaHany01 <youstinahany89@gmail.com>
2023-04-23 18:00:41 -0700

    done

--- main.h
+++ main.h
@@ -3,8 +3,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define OUTPUT_BUFF_SIZE 1024
+#define BUFF_FLUSH -1
+
 
 int _printf(const char *format, ...);
 
 
-#endif /* MAIN_H */
+#endif /* MAIN_H *

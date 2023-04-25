3976c778406e66a81521ff76e67185dcbc8fa770
YoustinaHany01 <youstinahany89@gmail.com>
2023-04-24 17:21:27 -0700

    done

--- _puts.c
+++ _puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * _puts - prints a string with newline
  * @str: the string to print
  *
  * Return: void
- *
  */
 int _puts(char *str)
 {
@@ -14,22 +14,24 @@ int _puts(char *str)
 		_putchar(*str++);
 	return (str - a);
 }
+
 /**
- * _putchar - writes thecharacter c to stdout
- * @c: the character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: on success 1.
- * on error, -1 is returned, and errno is set appropriately.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(int c)
 {
 	static int i;
-	static char buf[OUT_BUF_SIZE]
-		if (c == BUF_FLUSH || i >= OUT_BUF_SIZE)
-		{
-			write(1, buf, i);
-			i = 0;
-		}
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
 	if (c != BUF_FLUSH)
 		buf[i++] = c;
 	return (1);
